c39a50a5ac96109c6f7ebe1aaa5e9e63b84f38cf
SSkeptix <butsayura@gmail.com>
2019-01-02 20:54:39 +0200

    Fixed login page

--- StudentWebPortfolio.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ StudentWebPortfolio.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -87,7 +87,13 @@ namespace StudentWebPortfolio.Web.Areas.Identity.Pages.Account
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
                 var user = await _userManager.FindByEmailAsync(Input.Email);
-                var result = await _signInManager.PasswordSignInAsync(user, Input.Password, Input.RememberMe, lockoutOnFailure: true);
+				if (user == null)
+				{
+					ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+					return Page();
+				}
+
+				var result = await _signInManager.PasswordSignInAsync(user, Input.Password, Input.RememberMe, lockoutOnFailure: true);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
